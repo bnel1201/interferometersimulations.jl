5659cc6d26bfa0816edede09ec0f431c866917a2
Brandon Nelson <nelson.brandon@mayo.edu>
2021-07-10 18:49:53 -0500

    added coverage summary

---
+++ test/coverage/coverage-summary.jl
@@ -0,0 +1,7 @@
+import Pkg; Pkg.add("Coverage")
+using Coverage
+cd(joinpath(@__DIR__, "..", "..")) do
+    covered_lines, total_lines = get_summary(process_folder())
+    percentage = covered_lines / total_lines * 100
+    println("($(percentage)%) covered")
+end
