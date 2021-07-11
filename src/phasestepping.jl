050eb2051ec0ceb0eed874f6499d97d50fb23cda
Brandon Nelson <nelson.brandon@mayo.edu>
2021-07-11 12:28:23 -0500

    fixed sample_moire_pattern to not use nperiods

--- src/phasestepping.jl
+++ src/phasestepping.jl
@@ -4,20 +4,27 @@ export phase_step
 
 using ..Phantoms: PhaseContrastData
 
-function sample_moire_pattern(proj::PhaseContrastData, step_position=0; ref=false, moire_periods=5, ϕ=0, step_periods=1)
+"""
+sample_moire_pattern(proj::PhaseContrastData, step_position=0; ref=false, moire_periods=5, ϕ=0)
+
+step_position: x_n = n/N step position as fraction of 1 period
+ref: if true returns a reference moire pattern with no object
+ϕ: the system phase  (can be a scalar or 2D array)
+"""
+function sample_moire_pattern(proj::PhaseContrastData, step_position=0; ref=false, moire_periods=5, ϕ=0)
     @assert  step_position  >=  0
     M, N = size(proj)
     lateral_position = [i/M for i = range(1, M, step=1), j = range(1, N, step=1)];
     if ref
         proj = PhaseContrastData(0, 0, 1)
     end
-    return 1 .- proj.atten .+ proj.vis.*cos.(2*π*moire_periods.*(lateral_position) .+ 2*π*step_position/step_periods .+ proj.phase .+ ϕ)
+    return 1 .- proj.atten .+ proj.vis.*cos.(2*π*moire_periods.*(lateral_position) .+ 2*π*step_position .+ proj.phase .+ ϕ)
 end
 
 function phase_step(proj::PhaseContrastData; ref=false, nsteps=3, nperiods=1)
-    stepsize = 1/nsteps
-    steps = 0:stepsize:1-stepsize
-    return cat([sample_moire_pattern(proj, step; ref=ref, step_periods=nperiods) for step in steps]..., dims=3)
+    stepsize = nperiods/nsteps
+    steps = 0:stepsize:nperiods-stepsize
+    return cat([sample_moire_pattern(proj, step; ref=ref) for step in steps]..., dims=3)
 end
 
 end
--- test/phasestepping.jl
+++ test/phasestepping.jl
@@ -1,18 +1,27 @@
 ##
 using Test: @test
 using Statistics
+using FFTW
 using InterferometerSimulations
 
 ##
 true_proj = phantom_dataset()
-nperiods = 1
-ref = phase_step(true_proj, ref=true, nsteps=10, nperiods=nperiods)
-obj = phase_step(true_proj, nsteps=10, nperiods=nperiods)
+nperiods = 10
+nsteps = 40
+ref = phase_step(true_proj, ref=true, nsteps=nsteps, nperiods=nperiods)
+obj = phase_step(true_proj, nsteps=nsteps, nperiods=nperiods)
 
-@test (size(ref), size(obj)) == ((100, 100, 10), (100, 100, 10))
+@test (size(ref), size(obj)) == ((100, 100, nsteps), (100, 100, nsteps))
 
 @test minimum(ref) ≈ 0
 @test mean(ref) ≈ 1
 @test maximum(ref) ≈ 2
 
 @test mean(obj) < mean(ref)
+
+## test periods
+
+point = ref[1,1,:]
+ref_freq_response = abs.(fft(point)[2:Int(length(point)//2)])
+max_freq = argmax(ref_freq_response)
+@test max_freq == nperiods
