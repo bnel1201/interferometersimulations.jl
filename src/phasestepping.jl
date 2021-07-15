6b9b32d8dd5593da7087f8614c73be9655c08dc5
Brandon Nelson <nelson.brandon@mayo.edu>
2021-07-15 16:01:33 -0500

    moved 2pi and moire to lateral position

--- src/phasestepping.jl
+++ src/phasestepping.jl
@@ -5,19 +5,19 @@ export phase_step
 using ..Phantoms: PhaseContrastData
 
 """
-sample_moire_pattern(proj::PhaseContrastData, step_position=0; ref=false, moire_periods=5, ϕ=0)
+sample_moire_pattern(proj::PhaseContrastData, rel_step_position=0; ref=false, moire_periods=5, ϕ=0)
 
-step_position: x_n = n/N step position as fraction of 1 period
+rel_step_position: x_n = n/N step position as fraction of 1 period
 ref: if true returns a reference moire pattern with no object
 ϕ: the system phase  (can be a scalar or 2D array)
 """
-function sample_moire_pattern(proj::PhaseContrastData, step_position=0; ref=false, moire_periods=5, ϕ=0)
+function sample_moire_pattern(proj::PhaseContrastData, rel_step_position=0; ref=false, moire_periods=5, ϕ=0)
     M, N = size(proj)
-    lateral_position = [i/M for i = range(1, M, step=1), j = range(1, N, step=1)];
+    lateral_position = [2*π*moire_periods*i/M for i = range(1, M, step=1), j = range(1, N, step=1)];
     if ref
         proj = PhaseContrastData(0, 0, 1)
     end
-    return 1 .- proj.atten .+ proj.vis.*cos.(2*π*moire_periods.*(lateral_position) .+ 2*π*step_position .+ proj.phase .+ ϕ)
+    return 1 .- proj.atten .+ proj.vis.*cos.(2*π*rel_step_position .+ proj.phase .+ lateral_position .+ ϕ)
 end
 
 function phase_step(proj::PhaseContrastData; ref=false, nsteps=3, nperiods=1)
